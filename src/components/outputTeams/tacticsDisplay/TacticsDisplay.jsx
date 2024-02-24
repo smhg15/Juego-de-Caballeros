c3c3917b157a07cb0874ff09d0ed37c8b24fb386
Santiago  Hernandez <smhg15@gmail.com>
2024-02-24 18:12:21 +0100

    fix(input): fix input -10 y +22

--- src/components/outputTeams/tacticsDisplay/TacticsDisplay.jsx
+++ src/components/outputTeams/tacticsDisplay/TacticsDisplay.jsx
@@ -8,35 +8,39 @@ import Tactic10 from './tactics/Tactic10.jsx'
 import Tactic11 from './tactics/Tactic11.jsx'
  function TacticsDisplay({team}){
 
+    
     let tactic = <></>
 
-    switch(team.length){
-        case 5:
+    switch(true){
+
+        case team.length==0:
+            break
+        case team.length==5:
             tactic= <Tactic5 players={team}/>
             break
-        case 6:
+        case team.length==6:
             tactic= <Tactic6 players={team}/>
             break
-        case 7:
+        case team.length==7:
             tactic= <Tactic7 players={team}/>
             break
-        case 8:
+        case team.length==8:
             tactic= <Tactic8 players={team}/>
             break
-        case 9:
+        case team.length==9:
             tactic=  <Tactic9 players={team}/>
             break
-        case 10:
+        case team.length==10:
             tactic= <Tactic10 players={team}/>
             break
-        case 11:
+        case team.length==11:
             tactic= <Tactic11 players={team}/>
             break
-        case team.length <=4:
-            tactic= <Tactic11 players={team}/>
+        case team.length>=12:
+            tactic= <><Tactic11 players={team}/>{alert(`${team[11]} hace el asado`)}</>
             break
-        case team.length >=12:
-            tactic= <Tactic11 players={team}/>
+        default:
+            tactic= alert('...te falta gente maestro...\n (debe ingresar al menos 10 jugadores)')
             break
         
     }   
